7575c0d4e734da68865890281972789fd5c5fa5c
olicodea <olicodea@gmail.com>
2025-10-01 19:26:37 -0300

    configuración del context

--- src/context/DataContext.js
+++ src/context/DataContext.js
@@ -4,6 +4,8 @@ export const DataContext = createContext();
 
 export const useData = () => {
     const context = useContext(DataContext);
+
+    
     if (context === undefined)
         throw new Error("useData must be used within a DataProvider");
 
